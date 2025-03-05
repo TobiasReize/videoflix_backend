6e8c2ffbdb63583637b90a19db274f2bdfd427a2
Tobias Reize <reizeto@web.de>
2025-03-05 15:01:47 +0100

    added Video-Model to videoflix_app, updated settings.py and urls.py, added Video-Model to admin site

--- videoflix_app/models.py
+++ videoflix_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=255)
+    genres = models.JSONField()
+    thumbnail = models.FileField(upload_to='images/', blank=True, null=True)
+    video_file = models.FileField(upload_to='videos/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
