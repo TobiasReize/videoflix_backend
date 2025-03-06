b75d9eaf03f4a57f6c810a1c0c3d3ba158234615
Tobias Reize <reizeto@web.de>
2025-03-06 14:25:17 +0100

    updated tasks.py and added convert-function for 120p, 360p, 720p and 1080p, updated signals.py

--- videoflix_app/signals.py
+++ videoflix_app/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Video
-from .tasks import convert_480p
+from .tasks import convert_120p, convert_360p, convert_720p, convert_1080p
 import os
 
 
@@ -10,21 +10,24 @@ def video_post_save(sender, instance, created, **kwargs):
     print('Video saved')
     if created:
         print('New object created!')
-        convert_480p(instance.video_file.path)
+        convert_120p(instance.video_file.path)
+        convert_360p(instance.video_file.path)
+        convert_720p(instance.video_file.path)
+        convert_1080p(instance.video_file.path)
 
 
 @receiver(post_delete, sender=Video)
 def video_post_delete(sender, instance, **kwargs):
     original_path = instance.video_file.path
     base, ext = os.path.splitext(original_path)
-    converted_suffixes = ['_120p', '_360p', '_720p', '_1080p']
+    converted_resolutions = ['_120p', '_360p', '_720p', '_1080p']
 
     if os.path.isfile(original_path):
         os.remove(original_path)
         os.remove(instance.thumbnail.path)
 
-    for suffix in converted_suffixes:
-        converted_file = base + suffix + ext
+    for resolution in converted_resolutions:
+        converted_file = base + resolution + ext
         if os.path.isfile(converted_file):
             os.remove(converted_file)
             print(f'Deleted converted file: {converted_file}')
--- videoflix_app/tasks.py
+++ videoflix_app/tasks.py
@@ -2,11 +2,41 @@ import subprocess
 from os.path import splitext
 
 
-def convert_480p(source):
+def convert_120p(source):
     base, ext = splitext(source)
-    new_file = base + '_480p' + ext
-    cmd = 'C:\\usr\\ffmpeg\\bin\\ffmpeg.exe -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file)
-    result = subprocess.run(cmd, shell=True, capture_output=True)
-    print("Returncode:", result.returncode)
-    print("stdout:", result.stdout.decode())
-    print("stderr:", result.stderr.decode())
+    new_file = base + '_120p' + ext
+    cmd = 'C:\\usr\\ffmpeg\\bin\\ffmpeg.exe -i "{}" -s 214x120 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file)
+    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    print("Returncode_120p:", result.returncode)
+    print("stdout_120p:", result.stdout)
+    print("stderr_120p:", result.stderr)
+
+
+def convert_360p(source):
+    base, ext = splitext(source)
+    new_file = base + '_360p' + ext
+    cmd = 'C:\\usr\\ffmpeg\\bin\\ffmpeg.exe -i "{}" -s 640x360 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file)
+    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    print("Returncode_360p:", result.returncode)
+    print("stdout_360p:", result.stdout)
+    print("stderr_360p:", result.stderr)
+
+
+def convert_720p(source):
+    base, ext = splitext(source)
+    new_file = base + '_720p' + ext
+    cmd = 'C:\\usr\\ffmpeg\\bin\\ffmpeg.exe -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file)
+    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    print("Returncode_720p:", result.returncode)
+    print("stdout_720p:", result.stdout)
+    print("stderr_720p:", result.stderr)
+
+
+def convert_1080p(source):
+    base, ext = splitext(source)
+    new_file = base + '_1080p' + ext
+    cmd = 'C:\\usr\\ffmpeg\\bin\\ffmpeg.exe -i "{}" -s hd1080 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file)
+    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    print("Returncode_1080p:", result.returncode)
+    print("stdout_1080p:", result.stdout)
+    print("stderr_1080p:", result.stderr)
