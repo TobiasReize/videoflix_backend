8af8d8787d9f3c93826c3917c817e332fcb45a17
Tobias Reize <reizeto@web.de>
2025-03-06 17:51:58 +0100

    added CACHES-variable to settings

--- videoflix_backend/settings.py
+++ videoflix_backend/settings.py
@@ -110,6 +110,16 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "videoflix"
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
