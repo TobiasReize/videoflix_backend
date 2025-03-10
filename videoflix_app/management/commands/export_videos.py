921e9c9823d7244ba70ca0721148b46f9572a122
Tobias Reize <reizeto@web.de>
2025-03-10 07:55:04 +0100

    updated django-command export_videos.py, added backup-folder

--- videoflix_app/management/commands/export_videos.py
+++ videoflix_app/management/commands/export_videos.py
@@ -1,21 +1,18 @@
 from django.core.management.base import BaseCommand
 from videoflix_app.admin import VideoResource
-import datetime
+import datetime, os
 
 
 class Command(BaseCommand):
-    help = "Exportiert alle Videos als JSON und speichert sie als Backup."
+    help = 'Exportiert alle Videos als JSON und speichert sie als Backup.'
 
     def handle(self, *args, **kwargs):
         dataset = VideoResource().export()
         json_data = dataset.json
-
-        # Erstelle einen Dateinamen mit Zeitstempel
-        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-        filename = f"videos_backup_{timestamp}.json"
-
-        # Schreibe den JSON-String in die Datei
-        with open(filename, "w") as f:
+        timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+        filename = os.path.join('backup', f'{timestamp}_videos_backup.txt')
+        
+        with open(filename, 'w') as f:
             f.write(json_data)
 
-        self.stdout.write(self.style.SUCCESS(f"Backup gespeichert als {filename}"))
+        self.stdout.write(self.style.SUCCESS(f'Backup gespeichert als {filename}'))
