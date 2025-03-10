0a9804c76152a1433bd14398ac9b622e5b7924bc
TobiasReize <reizeto@web.de>
2025-03-10 16:02:13 +0100

    added postgresql database, updated settings

--- videoflix_backend/settings.py
+++ videoflix_backend/settings.py
@@ -108,11 +108,22 @@ WSGI_APPLICATION = 'videoflix_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'videoflix_db',
+		'USER': 'tobias',
+		'PASSWORD': 'test123',
+		'HOST': 'localhost',
+		'PORT': '',
+	}
 }
 
 CACHES = {
