d979608276d0c8d13e783d72927a2bb9b7b792ad
TobiasReize <reizeto@web.de>
2025-03-13 16:22:54 +0100

    updated settings

--- videoflix_backend/settings.py
+++ videoflix_backend/settings.py
@@ -75,13 +75,13 @@ MIDDLEWARE = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://127.0.0.1:5500',
-    'http://localhost:5500'
+    'http://127.0.0.1:4200',
+    'http://localhost:4200'
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:5500',
-    'http://localhost:5500'
+    'http://127.0.0.1:4200',
+    'http://localhost:4200'
 ]
 
 ROOT_URLCONF = 'videoflix_backend.urls'
