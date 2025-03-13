312ac3f3cbe4a83b561687faed24e0aafdb1e4bb
TobiasReize <reizeto@web.de>
2025-03-13 15:32:05 +0100

    added caching-layer to VideoListView

--- videoflix_app/api/views.py
+++ videoflix_app/api/views.py
@@ -1,8 +1,15 @@
 from rest_framework.generics import ListAPIView
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from django.conf import settings
 from ..models import Video
 from .serializers import VideoListSerializer
 
 
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
+
+@method_decorator(cache_page(CACHE_TTL), name='dispatch')
 class VideoListView(ListAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoListSerializer
