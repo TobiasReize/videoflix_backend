d33dde08eabd02f460caf938d030dce056f0081f
TobiasReize <reizeto@web.de>
2025-03-16 08:13:31 +0100

    added custom middleware for development

---
+++ videoflix_backend/middleware.py
@@ -0,0 +1,10 @@
+# for development:
+class AcceptRangesMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if request.path.startswith('/media/'):
+            response['Accept-Ranges'] = 'bytes'
+        return response
--- videoflix_backend/settings.py
+++ videoflix_backend/settings.py
@@ -68,6 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'videoflix_backend.middleware.AcceptRangesMiddleware',  # for development
 ]
 
 CSRF_TRUSTED_ORIGINS = [
