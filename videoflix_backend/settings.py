4ef24328c3479d32292fcbff14e46798f3c398b1
TobiasReize <reizeto@web.de>
2025-04-01 08:34:45 +0200

    updated settings

--- videoflix_backend/settings.py
+++ videoflix_backend/settings.py
@@ -34,7 +34,8 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     '49.13.218.194',
-    'videoflix-backend.tobias-reize.de'
+    'videoflix-backend.tobias-reize.de',
+    'videoflix.tobias-reize.de'
 ]
 
 INTERNAL_IPS = ['127.0.0.1']
@@ -78,14 +79,14 @@ MIDDLEWARE = [
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:4200',
     'http://localhost:4200',
-    'http://videoflix.tobias-reize.de',
+    'https://videoflix.tobias-reize.de',
     'http://videoflix-backend.tobias-reize.de'
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:4200',
     'http://localhost:4200',
-    'http://videoflix.tobias-reize.de',
+    'https://videoflix.tobias-reize.de',
     'http://videoflix-backend.tobias-reize.de'
 ]
 
