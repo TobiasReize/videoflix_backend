b9f1f8a78739088f20c7ca5dec8bf5a0929d0ca3
TobiasReize <reizeto@web.de>
2025-04-14 10:14:03 +0200

    added default-throttling

--- videoflix_backend/settings.py
+++ videoflix_backend/settings.py
@@ -202,7 +202,15 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication'
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '20/day',
+        'user': '100/day'
+    }
 }
 
 # Email
