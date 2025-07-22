12737ba26f1c42c6f2626a894c07b53659685b40
Tobias Reize <reizeto@web.de>
2025-07-22 11:12:01 +0200

    updated permission_classes in VideoListView

--- shared/permission.py
+++ shared/permission.py
@@ -1,18 +1,6 @@
 from rest_framework.permissions import BasePermission
 
 
-class IsAuthenticated(BasePermission):
-    """
-    Allows access if the user is authenticated.
-    """
-    def has_permission(self, request, view):
-        return request.user.is_authenticated
-
-
-    def has_object_permission(self, request, view, obj):
-        return request.user.is_authenticated
-
-
 class IsOwnerOrAdmin(BasePermission):
     """
     Allows access if the user is the administrator or owner.
--- videoflix_app/api/views.py
+++ videoflix_app/api/views.py
@@ -1,11 +1,11 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
 from django.conf import settings
 from ..models import Video
 from .serializers import VideoListSerializer
-from shared.permission import IsAuthenticated
 
 
 CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
